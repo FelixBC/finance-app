f69102638b7f92816994661f864f82131d395bab
Felix J. Blanco C. <Felixblancocabrera@gmail.com>
2026-02-20 12:09:06 -0400

    Mid day commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,50 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import InputHolder from "./components/InputHolder";
 
 function App() {
+  const [initial, setInitial] = useState<number>(0);
+  const [expectedReturn, setExpectedReturn] = useState<number>(0);
+  const [duration, setDuration] = useState<number>(0);
+  const [anualInvestment, setAnualInvestment] = useState<number>(0);
+  const type = "number";
+  const className = "card__input";
+
+  const handleInitial = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <div className="container">
         <Header className="card__header">Investment Calculator</Header>
-         <div className="card__button">
-          
-         </div>
+        <div className="card__input-container">
+          <InputHolder
+            className={className}
+            type={type}
+            value={initial}
+            onChange={}
+          />
+          <InputHolder
+            className={className}
+            type={type}
+            value={expectedReturn}
+            onChange={}
+          />
+          <InputHolder
+            className={className}
+            type={type}
+            value={duration}
+            onChange={}
+          />
+          <InputHolder
+            className={className}
+            type={type}
+            value={anualInvestment}
+            onChange={}
+          />
+        </div>
       </div>
     </>
   );
--- src/components/InputHolder.tsx
+++ src/components/InputHolder.tsx
@@ -1,25 +1,15 @@
 import { useState } from "react";
 
-type InputHolderProps = {};
+type InputHolderProps = {
+  type: string;
+  className: string;
+  value: number;
+  onChange: () => void;
+};
 
-const InputHolder: React.FC<InputHolderProps> = () => {
-  const [initial, setInitial] = useState<number>(0);
-  const [expectedReturn, setExpectedReturn] = useState<number>(0);
-  const [duration, setDuration] = useState<number>(0);
-  const [anualInvestment, setAnualInvestment] = useState<number>(0);
-
-  const handleInitial = () => {
-    setInitial((setInitial) => setInitial)
-
-  };
-
+const InputHolder: React.FC<InputHolderProps> = ({type, className, value, onChange}) => {
   return (
-    <div className="card__input-container">
-      <input type="number" className="card__input" value={initial} />
-      <input type="number" className="card__input" value={expectedReturn} />
-      <input type="number" className="card__input" value={duration} />
-      <input type="number" className="card__input" value={anualInvestment} />
-    </div>
+      <input type={type} className={className} value={value} onChange={onChange} />
   );
 };
 
