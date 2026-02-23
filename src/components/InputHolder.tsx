c80d5e0f758a9b3d312f2e4a8ff94432d91e753f
Felix J. Blanco C. <Felixblancocabrera@gmail.com>
2026-02-23 11:52:57 -0400

    feature: Inputs values section, mid day commit

--- src/components/InputHolder.tsx
+++ src/components/InputHolder.tsx
@@ -1,44 +1,77 @@
-import { useState } from "react";
+import { useState, type ReactEventHandler } from "react";
 
 type InputHolderProps = {
   onChange: () => void;
 };
 
-const InputHolder: React.FC<InputHolderProps> = ({ onChange }) => {
-  const [input, setInput] = useState({
+type userInputProps = {
+  initialInvestment: number;
+  annualInvestment: number;
+  expectedReturn: number;
+  duration: number;
+};
+
+const InputHolder: React.FC<InputHolderProps> = ({}) => {
+  const [userInput, setUserInput] = useState<userInputProps>({
     initialInvestment: 10000,
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10,
   });
 
+  const handleUserInput = (inputIdentifier: string, newValue: number) => {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  };
+
   return (
-    <>
-      <input
-        className="card__input"
-        type="number"
-        value={}
-        onChange={onChange}
-      />
-      <input
-        className="card__input"
-        type="number"
-        value={}
-        onChange={onChange}
-      />
-      <input
-        className="card__input"
-        type="number"
-        value={}
-        onChange={onChange}
-      />
-      <input
-        className="card__input"
-        type="number"
-        value={}
-        onChange={onChange}
-      />
-    </>
+    <section id="card__input-section">
+      <div>
+        <p>
+          <label>Initial Investment</label>
+          <input
+            className="card__input"
+            type="number"
+            value={userInput.initialInvestment}
+            onChange={onChange}
+          />
+        </p>
+        <p>
+          <label>Initial AnnualInvesment</label>
+          <input
+            className="card__input"
+            type="number"
+            value={userInput.annualInvestment}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              handleUserInput("initialInvestment", event.target.value)
+            }
+          />
+        </p>
+
+        <p>
+          <label>Duration</label>
+          <input
+            className="card__input"
+            type="number"
+            value={userInput.duration}
+            onChange={onChange}
+          />
+        </p>
+        <p>
+          <label>Expected Return</label>
+          <input
+            className="card__input"
+            type="number"
+            value={userInput.expectedReturn}
+            onChange={onChange}
+          />
+        </p>
+      </div>
+    </section>
   );
 };
 
